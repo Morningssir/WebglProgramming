b868c357caaf299f0ecb15b0e1c339451fb1ab33
morningssir <1977582074@qq.com>
2021-05-25 21:59:18 +0800

    Threejs chapter 8

---
+++ public/Threejs/js/index.js
@@ -0,0 +1,57 @@
+window.onload = function () {
+  const scene = new THREE.Scene();
+
+  // let mesh;
+
+  // 几何体
+  const geometry = new THREE.SphereGeometry(100, 25, 25);
+  geometry.scale(3, 3, 3);
+
+  const textureLoader = new THREE.TextureLoader();
+
+  textureLoader.load("./images/Earth.jpg", function (texture) {
+    const material = new THREE.MeshLambertMaterial({ map: texture });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    render();
+  });
+
+  // 光源对象
+  const ambient = new THREE.AmbientLight(0xffffff);
+  scene.add(ambient);
+  const directionalLight = new THREE.DirectionalLight(0xfffffff);
+  directionalLight.position.set(-1, 0, 0);
+  // directionalLight.target = mesh;
+  scene.add(directionalLight);
+  // spotLight.castShadow = true;
+  // spotLight.shadow.camera.near = 1;
+  // spotLight.shadow.camera.far = 1000;
+  // spotLight.shadow.camera.fov = 20;
+
+  // 相机对象
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const k = width / height;
+  const s = 200;
+  const camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
+  camera.position.set(200, 100, 200);
+  camera.lookAt(scene.position);
+
+  // 辅助对象
+  const axisHelper = new THREE.AxesHelper(250);
+  scene.add(axisHelper);
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(width, height);
+  renderer.setClearColor(0xb9d3ff, 1);
+  // renderer.shadowMap.enabled = true;
+  document.body.appendChild(renderer.domElement);
+
+  function render() {
+    // requestAnimationFrame(render);
+    renderer.render(scene, camera); //执行渲染操作
+  }
+  render();
+  var controls = new THREE.OrbitControls(camera, renderer.domElement); //创建控件对象
+  controls.addEventListener("change", render); //监听鼠标、键盘事件
+};
